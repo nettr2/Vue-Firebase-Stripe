5616422e650968c8d192c3413154b84aa156cfff
Rebecca Nettleton <nettr2@student.op.ac.nz>
2020-09-15 20:25:21 +1200

    Got the nav working

---
+++ vue-shop/src/main.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import jQuery from'jquery';
+
+window.$ = window.jQuery = jQuery;
+
+import 'popper.js';
+import 'bootstrap';
+import './assets/app.scss';
+
+Vue.component('NavBar',require('./components/NavBar.vue').default);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
